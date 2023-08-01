c119fc6ecf2012bfe33eb7e701707adeea0f1585
Mehmet ali <maliyildiz0307@gmail.com>
2023-08-02 01:34:18 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./reset.css";
-import { ChakraProvider } from '@chakra-ui/react' //chakra ui provider ve tema .
+import { ChakraProvider } from '@chakra-ui/react' 
 import { QueryClient, QueryClientProvider } from 'react-query';
 import {ReactQueryDevtools} from "react-query/devtools";
 
@@ -15,14 +15,14 @@ import  {AuthProvider} from './Context/AuthContext';
 import { BasketProvider } from './Context/BasketContext';
 
 
-const queryClient = new QueryClient({ //react query için hazır çekiyoruz.
+const queryClient = new QueryClient({ 
   defaultOptions:{
     queries:{
-      refetchOnMount:false, //başka bir ekrana gidip geri geldiğimizde ,tekrar cachelediği veri olsa da veriyi tekrar çekmesinin önüne geçer.
-      refetchOnWindowFocus:false, //burasıda aynı mantıkla başka bir window a gittikten sonra tekrar veri çekilmesinin önüne geçer.
+      refetchOnMount:false, 
+      refetchOnWindowFocus:false, 
     }
   }
-}); //react query kullanarak db ye bağlanıp state yönetme işlemleri için .
+}); 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -35,12 +35,10 @@ root.render(
           </BasketProvider>
         </AuthProvider>
       </ChakraProvider>
-      <ReactQueryDevtools initialIsOpen={false} /> {/* developer tool u sol altta gösterir*/}
+      <ReactQueryDevtools initialIsOpen={false} /> 
     </QueryClientProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
